d7abe492e505af9350b6a6bf26c1589cd2650a98
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-15 12:55:42 +0530

    Added routing code

--- src/components/common/NavbarComponent.js
+++ src/components/common/NavbarComponent.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './NavbarComponent.css';
 import DarkMode from './DarkMode/DarkMode';
+import { Link } from 'react-router-dom';
 
 function NavbarComponent() {
 
@@ -12,8 +13,9 @@ function NavbarComponent() {
     <Navbar expand="lg" className={`navbar-dark navbar-custom `}>
       <Container fluid>
         <Nav className='ml-auto nav-collap'>
-          <Nav.Link href="#action2" className='nav-link'>sign-up</Nav.Link>
-          <Nav.Link href="#action2" className='nav-link'>Login</Nav.Link>
+          <Link to="/" className='nav-link'>Home</Link>
+          <Link to="/Signup" className='nav-link'>sign-up</Link>
+          <Link to="/Login" className='nav-link'>Login</Link>
         </Nav>
         <DarkMode/>
       </Container>
