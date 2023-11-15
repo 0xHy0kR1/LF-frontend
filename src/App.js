2e4ece4c872c1d90cb181072a9ba6dfae17c88b0
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-15 12:58:09 +0530

    Added code for routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavbarComponent from './components/common/NavbarComponent'; // Correct import path
 import Home from './components/pages/Home';
 import './index.css'
+import Signup from './components/authentication/Signup';
+import Login from './components/authentication/Login';
 // import LostItems from './components/pages/LostItems';
 // import FoundItems from './components/pages/FoundItems';
 // import ItemDetails from './components/pages/ItemDetails';
@@ -15,6 +17,8 @@ function App() {
       <NavbarComponent />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Login" element={<Login />} />
           {/* <Route path="/lost" element={<LostItems />} /> */}
           {/* <Route path="/found" element={<FoundItems />} /> */}
           {/* <Route path="/item/:id" element={<ItemDetails />} /> */}
