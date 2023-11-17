60a5e801fcb64c4fc75459d19bf8b7fa86ff07ab
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-17 15:48:15 +0530

    Added code to send cred to backend as well as added Alert stuffs to show alert

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,21 +1,59 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css'; 
-import top from "./../../assets/cred-dark.png";
+import top from "./../../assets/cred-top.png";
 
-function Login() {
+function Login(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alert, setAlert] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Prepare the data to be sent to the backend
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    // Make a POST request to the backend
+    try{
+      const response = await fetch('http://localhost:5000/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+
+      if(response.ok){
+        // Show success alert
+        props.showAlert('success', 'Login successful');
+      }
+      else{
+        // Show error alert
+        props.showAlert('danger', 'Login failed');
+      }
+    } catch(error){
+      console.error('Error: ' + error);
+      // Show error alert 
+      props.showAlert('danger', 'Login failed');
+    }
+  }
+
   return (
     <Container className='login-container'>
-      <Form className='login-form'>
+      <Form className='login-form' onSubmit={handleSubmit}>
         <img src={top} alt="cred-dark" className='top-img'/>
         <Form.Group className="mb-4 login-input-block" controlId="formBasicEmail">
-          <Form.Control className="login-input" type="email" placeholder="email address" />
+          <Form.Control className="login-input" type="email" placeholder="email address" onChange={(e) => setEmail(e.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-4 login-input-block" controlId="formBasicPassword">
-          <Form.Control className="login-input" type="password" placeholder="password" />
+          <Form.Control className="login-input" type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
         </Form.Group>
 
         <Button variant="success" className="login-submit-button" type="submit">
--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -3,23 +3,54 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import React from 'react';
 import './Signup.css'; 
-import top from "./../../assets/cred-dark.png";
+import top from "./../../assets/cred-top.png";
 
-function Signup() {
+function Signup(props) {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Get form data
+    const formData = {
+      username: e.target.username.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+
+    // Send the form data to the backend
+    try{
+      const response = await fetch('http://localhost:5000/api/auth/createuser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+
+      if(response.ok){
+        props.showAlert('success', 'Login successful');
+      }
+      else{
+        props.showAlert('danger', 'Login failed');
+      }
+    }
+  catch(error){
+    props.showAlert('danger', 'Login failed');
+  }
+}
   return (
     <Container className='signup-container'>
-      <Form className='signup-form'>
+      <Form className='signup-form' onSubmit={handleSubmit}>
       <img src={top} alt="cred-dark" className='top-img'/>
         <Form.Group className="mb-4 signup-input-block" controlId="formBasicEmail">
-          <Form.Control className="signup-input" type="text" placeholder="username" />
+          <Form.Control className="signup-input" name="username" type="text" placeholder="username" />
         </Form.Group>
 
         <Form.Group className="mb-4 signup-input-block" controlId="formBasicEmail">
-          <Form.Control className="signup-input" type="email" placeholder="email address" />
+          <Form.Control className="signup-input" type="email" placeholder="email address" name="email"/>
         </Form.Group>
 
         <Form.Group className="mb-4 signup-input-block" controlId="formBasicPassword">
-          <Form.Control className="signup-input" type="password" placeholder="password" />
+          <Form.Control className="signup-input" type="password" placeholder="password" name="password"/>
         </Form.Group>
 
         <Button variant="success" className="signup-submit-button" type="submit">
