cf1d267c258b039c184a456b179ccf3ae47b891d
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-17 15:50:42 +0530

    Added Alert comment stuffs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavbarComponent from './components/common/NavbarComponent'; // Correct import path
@@ -6,19 +6,33 @@ import Home from './components/pages/Home';
 import './index.css'
 import Signup from './components/authentication/Signup';
 import Login from './components/authentication/Login';
+import Alert from './components/common/Alert'
 // import LostItems from './components/pages/LostItems';
 // import FoundItems from './components/pages/FoundItems';
 // import ItemDetails from './components/pages/ItemDetails';
 // import Footer from './components/common/Footer';
 
 function App() {
+  const [alert, setAlert] = useState(null);
+
+  // Function to show an alert
+  const showAlert = (type, message) => {
+    setAlert({
+      type: type,
+      msg: message
+    });
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
   return (
     <BrowserRouter>
       <NavbarComponent />
+      <Alert alert={alert} />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/Login" element={<Login />} />
+          <Route path="/" element={<Home />} /> 
+          <Route path="/Signup" element={<Signup showAlert={showAlert} />} /> // Pass showAlert function to Signup component
+          <Route path="/Login" element={<Login showAlert={showAlert} />} />
           {/* <Route path="/lost" element={<LostItems />} /> */}
           {/* <Route path="/found" element={<FoundItems />} /> */}
           {/* <Route path="/item/:id" element={<ItemDetails />} /> */}
