273df887fbe18509528a9fcf1628095d9bd4b98c
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-17 15:51:17 +0530

    Alert component to show an alert

---
+++ src/components/common/Alert.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function Alert(props) {
+  const [show, setShow] = useState(true);
+  const capitalized = (word) => {
+    if (!word) {
+        return ''; // Return an empty string or another default value
+      }
+    if(word === "danger"){
+      word = "error";
+    }
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  }
+
+  return (
+    <div style={{height:'50px'}}>
+      {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        <strong>{capitalized(props.alert.type)}</strong>: {props.alert.msg}
+      </div>}
+    </div>
+  )
+}
+
+export default Alert;
