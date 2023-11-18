d17ade4aa57f7c120dd059317da8afad95788181
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-18 16:58:36 +0530

    Added Logout button and show links according to user login status

--- src/components/common/NavbarComponent.js
+++ src/components/common/NavbarComponent.js
@@ -5,18 +5,45 @@ import Navbar from 'react-bootstrap/Navbar';
 import './NavbarComponent.css';
 import DarkMode from './DarkMode/DarkMode';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
 
 function NavbarComponent() {
 
+  const navigate = useNavigate();
+
+  // Check if the user is logged in (based on the presence of authToken)
+  const isLoggedIn = !!localStorage.getItem('authToken');
+
+  // Handle logout
+  const handleLogout = () => {
+    // Clear the authentication token from localStorage
+    localStorage.removeItem('authToken');
+
+    // Redirect the user to the login page
+    navigate('/login');
+  };
 
   return (
     <Navbar bg="dark" data-bs-theme="dark">
     <Container fluid >
       <Nav className="me-auto">
       <Link to="/" className='nav-link'>Home</Link>
-      <Link to="/Signup" className='nav-link'>sign-up</Link>
-      <Link to="/Login" className='nav-link'>Login</Link>
+      {isLoggedIn ? (
+            <>
+              <Link to="/lost" className='nav-link'>Lost Items</Link>
+              <Link to="/found" className='nav-link'>Found Items</Link>
+            </>
+          ) : (
+            <>
+              <Link to="/Signup" className='nav-link'>sign-up</Link>
+              <Link to="/Login" className='nav-link'>Login</Link>
+            </>
+      )}
       </Nav>
+      {isLoggedIn && (
+        <Button variant='danger' className='logout-button' onClick={handleLogout}>Logout</Button>
+      )}
       <DarkMode/>
     </Container>
   </Navbar>
