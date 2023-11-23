5d47ad7cf2d343bddcb80344947a64dde64600d5
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-23 07:00:33 +0530

    Added Scroll button

--- src/components/pages/LostItems.js
+++ src/components/pages/LostItems.js
@@ -1,174 +1,199 @@
-import React, { useEffect, useState } from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import { getLostItems, deleteLostItem, answerSecurityQuestion, viewLostItem } from './../services/lostItemService';
-import SecurityQuestionModal from './../modal/SecurityQuestionModal';
-import './LostItems.css';
-import PostItemModal from "../modal/PostItemModal";
-import Spinner from './../common/Spinner';
+  import React, { useEffect, useState } from 'react';
+  import Card from 'react-bootstrap/Card';
+  import Button from 'react-bootstrap/Button';
+  import Modal from 'react-bootstrap/Modal';
+  import { getLostItems, deleteLostItem, answerSecurityQuestion, viewLostItem } from './../services/lostItemService';
+  import SecurityQuestionModal from './../modal/SecurityQuestionModal';
+  import './LostItems.css';
+  import PostItemModal from "../modal/PostItemModal";
+  import Spinner from './../common/Spinner';
 
-const LostItems = (props) => {
+  const LostItems = (props) => {
 
-  const [lostItems, setLostItems] = useState([]);
-  const [selectedItemId, setSelectedItemId] = useState(null);
-  const [showSecurityQuestionModal, setShowSecurityQuestionModal] = useState(false);
-  const [showEmail, setShowEmail] = useState(false);
-  const [userEmail, setUserEmail] = useState('');
-  const [securityQuestion, setSecurityQuestion] = useState('');
-  const [loading, setLoading] = useState(true);
+    const [lostItems, setLostItems] = useState([]);
+    const [selectedItemId, setSelectedItemId] = useState(null);
+    const [showSecurityQuestionModal, setShowSecurityQuestionModal] = useState(false);
+    const [showEmail, setShowEmail] = useState(false);
+    const [userEmail, setUserEmail] = useState('');
+    const [securityQuestion, setSecurityQuestion] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [scrollY, setScrollY] = useState(0);
 
-  // State to manage modal visibility 
-  const [showModal, setShowModal] = useState(false);
+    // State to manage modal visibility 
+    const [showModal, setShowModal] = useState(false);
 
-  // Function to show the modal 
-  const handleModalShow = () => setShowModal(true);
+    // Function to show the modal 
+    const handleModalShow = () => setShowModal(true);
 
-  // Function to hide the modal
-  const handleModalClose = () => setShowModal(false);
+    // Function to hide the modal
+    const handleModalClose = () => setShowModal(false);
 
-  const fetchLostItems = async () => {
-    try{
-        setLoading(true); // Set loading to true when fetching starts
-        const response = await getLostItems();
-        const items = response.lostItems; // Access the lostItems property
-        setLostItems(items);
-        console.log(items);
-        setLoading(false); // Set loading to false when fetching is complete
-    } catch (error){
-        console.error('Error fetching lost items:', error);
-        props.showAlert('danger', 'Error fetching lost items');
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
     }
-  };
-
-  useEffect(() => {
-    // Fetch and set the list of lost items when the component mounts
-    fetchLostItems();
-  }, []);
-
-  const handleDeleteItem = async (itemId) => {
-    try{
-        const deleteResult = await deleteLostItem(itemId);
-        // Remove the deleted item from the local state
-        setLostItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
-        
-        if(deleteResult.success) {
-            props.showAlert('success', deleteResult.message);
-        } else{
-            props.showAlert('danger', deleteResult.message);
-        }
-    } catch(error){
-        console.error('Error deleting lost item:', error);
-        props.showAlert('danger', 'Failed to delete the lost item');
-    }
-  }
-
-  const handleViewDetails = async (itemId) => {
-    let itemDetails;
-    try{
-        itemDetails = await viewLostItem(itemId);
-        // Display item details
-        console.log('Item details:', itemDetails);
-        if(itemDetails.data.securityQuestion){
-          setShowEmail(false);
-          setSelectedItemId(itemId);
-          setSecurityQuestion(itemDetails.data.securityQuestion);
-          setShowSecurityQuestionModal(true);
-        } else{
-          setShowSecurityQuestionModal(false);
-        }
-    } catch(error){
-        console.error('Error viewing item details:', error);
-        props.showAlert('danger', 'Error viewing item details');
-    }
-  }
-
-  const handleAnswerSecurityQuestion = async (itemId, answer) => {
-    try{
-      const result = await answerSecurityQuestion(itemId, { securityQuestion: { answer}});
-      if(result.data && result.data.email){
-        setUserEmail(result.data.email);
-        setShowEmail(true);
-        setShowSecurityQuestionModal(false);
-      }else {
-        setShowSecurityQuestionModal(false);
-        props.showAlert('danger', result.message || 'Incorrect answer');
+    const fetchLostItems = async () => {
+      try{
+          setLoading(true); // Set loading to true when fetching starts
+          const response = await getLostItems();
+          const items = response.lostItems; // Access the lostItems property
+          setLostItems(items);
+          console.log(items);
+          setLoading(false); // Set loading to false when fetching is complete
+      } catch (error){
+          console.error('Error fetching lost items:', error);
+          props.showAlert('danger', 'Error fetching lost items');
       }
-    } catch(error){
-      props.showAlert('danger', "Incorrect answer");
-      console.error('Error answering security question', error);
-    }
-  }
-  const handleCloseSecurityQuestionModal = () => {
-    setShowSecurityQuestionModal(false);
-  };
+    };
 
-  const handleCloseEmailModal = () => {
-    setShowEmail(false);
-    setUserEmail('');
-  }
-  return (
-     <div>
-      <h2 className='text-center'>Lost Items</h2>
-      <PostItemModal
-            showModal={showModal}
-            handleModalShow={handleModalShow}
-            handleModalClose={handleModalClose}
-            fetchLostItems={fetchLostItems}
+    useEffect(() => {
+      // Fetch and set the list of lost items when the component mounts
+      fetchLostItems();
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+
+    }, []);
+
+    const handleDeleteItem = async (itemId) => {
+      try{
+          const deleteResult = await deleteLostItem(itemId);
+          // Remove the deleted item from the local state
+          setLostItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+          
+          if(deleteResult.success) {
+              props.showAlert('success', deleteResult.message);
+          } else{
+              props.showAlert('danger', deleteResult.message);
+          }
+      } catch(error){
+          console.error('Error deleting lost item:', error);
+          props.showAlert('danger', 'Failed to delete the lost item');
+      }
+    }
+
+    const handleViewDetails = async (itemId) => {
+      let itemDetails;
+      try{
+          itemDetails = await viewLostItem(itemId);
+          // Display item details
+          console.log('Item details:', itemDetails);
+          if(itemDetails.data.securityQuestion){
+            setShowEmail(false);
+            setSelectedItemId(itemId);
+            setSecurityQuestion(itemDetails.data.securityQuestion);
+            setShowSecurityQuestionModal(true);
+          } else{
+            setShowSecurityQuestionModal(false);
+          }
+      } catch(error){
+          console.error('Error viewing item details:', error);
+          props.showAlert('danger', 'Error viewing item details');
+      }
+    }
+
+    const handleAnswerSecurityQuestion = async (itemId, answer) => {
+      try{
+        const result = await answerSecurityQuestion(itemId, { securityQuestion: { answer}});
+        if(result.data && result.data.email){
+          setUserEmail(result.data.email);
+          setShowEmail(true);
+          setShowSecurityQuestionModal(false);
+        }else {
+          setShowSecurityQuestionModal(false);
+          props.showAlert('danger', result.message || 'Incorrect answer');
+        }
+      } catch(error){
+        props.showAlert('danger', "Incorrect answer");
+        console.error('Error answering security question', error);
+      }
+    }
+    const handleCloseSecurityQuestionModal = () => {
+      setShowSecurityQuestionModal(false);
+    };
+
+    const handleCloseEmailModal = () => {
+      setShowEmail(false);
+      setUserEmail('');
+    }
+
+    const handleScrollToTop = () => {
+      window.scrollTo({top: 0, behavior: 'smooth'});
+    };
+
+    return (
+      <div>
+        <h2 className='text-center'>Lost Items</h2>
+        <PostItemModal
+              showModal={showModal}
+              handleModalShow={handleModalShow}
+              handleModalClose={handleModalClose}
+              fetchLostItems={fetchLostItems}
+            />
+        <Button
+              variant='primary'
+              className="post-item-button"
+              onClick={handleModalShow}
+            >
+              Post Item
+        </Button>
+        {loading && <Spinner />} {/* Render the spinner while loading */}
+        <div className="cards">
+          {!loading && lostItems.map((item) => (
+            <Card key={item._id} className="custom-card" >
+              <Card.Img className="card-image" variant="top" src={item.imageUrl} alt={item.title} />
+              <Card.Body className='card-body'>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>Description: {item.description}</Card.Text>
+                <Card.Text>Category: {item.category}</Card.Text>
+                <Card.Text>Location: {item.location}</Card.Text>
+                {item.securityQuestion && (
+                  <Button variant="info" onClick={() => handleViewDetails(item._id)}>
+                    View Email of author
+                  </Button>
+                )}
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+
+        {/* Modal for answering security question */}
+        {showSecurityQuestionModal && securityQuestion && ( // Check if itemDetails is defined
+          <SecurityQuestionModal
+            show={showSecurityQuestionModal}
+            handleClose={handleCloseSecurityQuestionModal}
+            onSubmit={(answer) => handleAnswerSecurityQuestion(selectedItemId, answer)}
+            securityQuestion={securityQuestion}
           />
-      <Button
-            variant='primary'
-            className="post-item-button"
-            onClick={handleModalShow}
-          >
-            Post Item
-      </Button>
-      {loading && <Spinner />} {/* Render the spinner while loading */}
-      <div className="cards">
-        {!loading && lostItems.map((item) => (
-          <Card key={item._id} className="custom-card" >
-            <Card.Img className="card-image" variant="top" src={item.imageUrl} alt={item.title} />
-            <Card.Body className='card-body'>
-              <Card.Title>{item.title}</Card.Title>
-              <Card.Text>Description: {item.description}</Card.Text>
-              <Card.Text>Category: {item.category}</Card.Text>
-              <Card.Text>Location: {item.location}</Card.Text>
-              {item.securityQuestion && (
-                <Button variant="info" onClick={() => handleViewDetails(item._id)}>
-                  View Email of author
-                </Button>
-              )}
-            </Card.Body>
-          </Card>
-        ))}
+        )}
+
+        {/* Modal for displaying the email */}
+        <Modal show={showEmail} onHide={handleCloseEmailModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>User Email</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Here's the author's email: {userEmail}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseEmailModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+      {scrollY > 0 && (
+        <Button
+          variant='secondary'
+          className='scroll-to-top-button'
+          onClick={handleScrollToTop}
+        >
+          &#9650;
+        </Button>
+      )}
       </div>
 
-      {/* Modal for answering security question */}
-      {showSecurityQuestionModal && securityQuestion && ( // Check if itemDetails is defined
-        <SecurityQuestionModal
-          show={showSecurityQuestionModal}
-          handleClose={handleCloseSecurityQuestionModal}
-          onSubmit={(answer) => handleAnswerSecurityQuestion(selectedItemId, answer)}
-          securityQuestion={securityQuestion}
-        />
-      )}
+    )
+  }
 
-      {/* Modal for displaying the email */}
-      <Modal show={showEmail} onHide={handleCloseEmailModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>User Email</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Here's the author's email: {userEmail}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseEmailModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  )
-}
-
-export default LostItems
+  export default LostItems
