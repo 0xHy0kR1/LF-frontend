ee95b145cd7a2351b089fd1cfebb870cda73ebde
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-25 06:47:41 +0530

    Added function to fetch lost items

---
+++ src/utils/lostItemUtils.js
@@ -0,0 +1,22 @@
+// lostItemUtils.js
+import { getLostItems } from './../components/services/lostItemService'; // Import the actual service function
+
+export const fetchLostItems = async (setLoading, setLostItems, showAlert) => {
+  try {
+    setLoading(true); // Set loading to true when fetching starts
+
+    // Simulate a delay (e.g., 1 second) before fetching data
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const response = await getLostItems(); // Adjust this based on your actual API call
+    const items = response.lostItems; // Access the lostItems property
+    setLostItems(items);
+    console.log(items);
+
+    setLoading(false); // Set loading to false when fetching is complete
+  } catch (error) {
+    console.error('Error fetching lost items:', error);
+    showAlert('danger', 'Error fetching lost items');
+    setLoading(false); // Make sure to set loading to false in case of an error
+  }
+};
