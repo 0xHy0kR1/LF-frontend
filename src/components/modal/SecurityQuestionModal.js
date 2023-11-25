a192ae466319402773d17d755d3381657ff7f83c
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-25 06:39:54 +0530

    Added some checks to validate presence of answer and some functions

--- src/components/modal/SecurityQuestionModal.js
+++ src/components/modal/SecurityQuestionModal.js
@@ -2,28 +2,41 @@ import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
+import './SecurityQuestionModal.css'
 const SecurityQuestionModal = (props) => {
   
   const [ answer, setAnswer] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
+    // Check if the answer is present
+    if (!answer) {
+      // Show an alert if the answer is empty
+      props.showAlert('danger', 'Please fill in all required fields.');
+      props.handleClose();
+      return;
+    }
+
+    // Call the parent component's onSubmit function
     props.onSubmit(answer);
   };
 
   return (
-    <Modal show={props.show} onHide={props.handleClose}>
-      <Modal.Header closeButton>
+    <Modal show={props.show} onHide={props.handleClose} >
+      <Modal.Header className='security-answer-modal'>
         <Modal.Title>Answer Security Question</Modal.Title>
+        <Button variant="link" className="security-question-close-button" onClick={props.handleClose}>
+          X
+        </Button>
       </Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="formSecurityQuestion">
+      <Modal.Body className='security-answer-modal'>
+        <Form onSubmit={handleSubmit} className='main-box'>
+          <Form.Group controlId="formSecurityQuestion" className='answer-question-box'>
             <Form.Label>{props.securityQuestion}</Form.Label>
-            <Form.Control type="text" placeholder="Enter your answer" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+            <Form.Control type="text" className='answer-modal-input' placeholder="Enter your answer" value={answer} onChange={(e) => setAnswer(e.target.value)} />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" className='security-question-submit-button'>
             Submit
           </Button>
         </Form>
