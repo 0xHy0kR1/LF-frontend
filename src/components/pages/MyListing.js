386a3c9aa109d3d0b420bcdb9c5fac1cf9ed7243
0xHy0kR1 <helloweenhell666@gmail.com>
2023-11-30 18:49:59 +0530

    made the general logic for displaying toast

--- src/components/pages/MyListing.js
+++ src/components/pages/MyListing.js
@@ -5,8 +5,9 @@ import Toast from 'react-bootstrap/Toast';
 import Button from 'react-bootstrap/Button';
 import { jwtDecode } from "jwt-decode";
 import { fetchLostItems } from './../../utils/lostItemUtils';
+import { markItemAsFound } from './../services/lostItemService'; // Import the actual service function
 
-const MyListing = ({ showAlert, lostItems, setLostItems, loading, setLoading }) => {
+const MyListing = ({ showAlert, lostItems, setLostItems, loading, setLoading}) => {
 
   useEffect(() => {
     // Fetch lost items for the current user when the component mounts
@@ -22,24 +23,67 @@ const MyListing = ({ showAlert, lostItems, setLostItems, loading, setLoading }) 
     fetchData(); // Call the fetchData function to initiate the data fetching
   }, []);
 
-  // Function to handle the "YES" button click in the toast
-  const handleToastYesClick = () => {
-    // Add logic to update the status of the item to "found" on the backend
-    // Then, fetch and update the local state with the new found items
-    // Display a success message using showAlert
-  };
-
   // Get the current user's ID from your authToken 
   const authToken = localStorage.getItem('authToken');
   const decodedToken = jwtDecode(authToken);
   console.log("decoded token value: ",decodedToken);
   const currentUserId = decodedToken.userId; // Adjust this based on your implementation
   console.log("current user id: ",currentUserId);
-
   // Filter lost items based on the current user
   console.log("item value: ",lostItems);
   const userLostItems = lostItems.filter((item) => item.user === currentUserId);
 
+  console.log("userLostItems value: ", userLostItems)
+
+  const shouldDisplayToast = localStorage.getItem('userId') === currentUserId;
+  // const isMyListingUserEqualToLostItemUser = 
+    // Function to handle the "YES" button click in the toast(Adding logic to update the status of the item to "found" on the backend)
+    const handleToastYesClick = async () => {
+      
+      console.log("inside handleToastYesClick");
+      // Filter lost items based on the current user and displayTitle
+      console.log("currentUserId value: " + currentUserId);
+
+      // get the displayTitle from the localStorage
+      const displayTitle = localStorage.getItem("displayTitle");
+
+      const markFoundItem = lostItems.filter((item) => item.title === displayTitle);
+      console.log("markFoundItem value: " + JSON.stringify(markFoundItem))
+      console.log("markFoundItem object value: " + markFoundItem);
+      
+      if(markFoundItem.length > 0){
+        const itemId = markFoundItem[0]._id; 
+        console.log("itemId value: " + itemId);
+        try{
+          // Call the function to mark the item as found
+          const markAsFoundResult = await markItemAsFound(itemId);
+          console.log("markAsFoundResult value: " + JSON.stringify(markAsFoundResult));
+          if(markAsFoundResult.success){
+  
+          
+            // Update the local state with the new found items
+            await fetchLostItems(setLoading, setLostItems, showAlert);
+  
+            // Display a success message using showAlert
+            showAlert('success', markAsFoundResult.message);
+            
+            // Remove displayTitle from localStorage
+            localStorage.removeItem('displayTitle');
+
+            // Hide the toast(Persist the state in localStorage)
+            localStorage.setItem('securityQuestionAnswered', 'false');
+          } else{
+            // Handle the case where marking the item as found failed
+            console.error(markAsFoundResult.message);
+            showAlert('danger', markAsFoundResult.message);
+          }
+        } catch (error){
+          // Handling any unexpected errors
+          console.error('Failed to mark the item as found: ', error);
+          showAlert('danger', 'Failed to mark the item as found');
+        }
+      };
+    }
   return (
     <div>
       <h2 className='text-center'>My Listings</h2>
@@ -60,9 +104,9 @@ const MyListing = ({ showAlert, lostItems, setLostItems, loading, setLoading }) 
       {/* <FoundItems /* Similar to LostItems component */ /*/> */}
 
       {/* Toast for "Did you get the item" */}
-      <Toast /* Implement toast logic with 'handleToastYesClick' */>
+      <Toast show={(localStorage.getItem('securityQuestionAnswered') === 'true') && shouldDisplayToast} >
         <Toast.Header closeButton={false}>
-          <strong>Did you get the 'item name'?</strong>
+          <strong>Did you get the {localStorage.getItem('displayTitle')}?</strong>
         </Toast.Header>
         <Toast.Body>
           <Button variant='success' onClick={handleToastYesClick}>
@@ -72,6 +116,6 @@ const MyListing = ({ showAlert, lostItems, setLostItems, loading, setLoading }) 
       </Toast>
     </div>
   );
-};
+}
 
 export default MyListing;
