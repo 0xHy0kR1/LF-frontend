a6f72c253af91c56e9c571f9f1a97b4af295fff3
0xHy0kR1 <helloweenhell666@gmail.com>
2024-02-10 14:36:27 +0530

    Added to check token expiration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
 import NavbarComponent from "./components/common/NavbarComponent"; // Correct import path
 import Home from "./components/pages/Home";
 import "./index.css";
 import Signup from "./components/authentication/Signup";
 import Login from "./components/authentication/Login";
-// import Alert from './components/common/Alert'
 import LostItems from "./components/pages/LostItems";
 import FoundItems from "./components/pages/FoundItems";
 import MyListing from "./components/pages/MyListing.js";
@@ -14,9 +13,8 @@ import { fetchLostItems } from "./utils/lostItemUtils";
 import { fetchFoundItems } from "./utils/foundItemUtils.js";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { motion } from "framer-motion";
-// import ItemDetails from './components/pages/ItemDetails';
-// import Footer from './components/common/Footer';
+import { motion, AnimatePresence } from "framer-motion";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
   const [lostItems, setLostItems] = useState([]);
@@ -25,6 +23,7 @@ function App() {
   const [foundItems, setFoundItems] = useState([]);
   const [burgerClicked, setBurgerClicked] = useState(false);
 
+  const location = useLocation();
   const handleBurgerClick = () => {
     setBurgerClicked(!burgerClicked);
   };
@@ -49,8 +48,32 @@ function App() {
     await fetchLostItems(setLostLoading, setLostItems);
   };
 
+  useEffect(() =>{
+    
+    // Check token expiration on mount
+    checkTokenExpiration();
+  }, []);
+
+  const checkTokenExpiration = () => {
+    const authToken = localStorage.getItem('authToken');
+
+    if(authToken) {
+
+      try{
+        const decodedToken = jwtDecode(authToken); 
+        const currentTime = Date.now()/1000;
+
+        console.log("token expiry time", decodedToken.exp);
+        console.log("Current time: " + currentTime)
+        if(decodedToken.exp < currentTime) {
+          window.location.href = '/login'
+        }
+      } catch(error){
+        console.error('Error decoding JWT:', error);
+      }
+    }
+  }
   return (
-    <BrowserRouter>
     <motion.div
         initial={{ opacity: 0, y: "-100vh" }}
         animate={{ opacity: 1, y: 0 }}
@@ -70,54 +93,53 @@ function App() {
           <>
             {/* <Alert alert={alert} /> */}
             <ToastContainer position="top-right" autoClose={2000} />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/Signup" element={<Signup />} />
-              <Route path="/Login" element={<Login />} />
-              <Route
-                path="/lost"
-                element={
-                  <LostItems
-                    setLostItems={setLostItems}
-                    setLostLoading={setLostLoading}
-                    lostItems={lostItems}
-                    lostLoading={lostLoading}
-                  />
-                }
-              />
-              <Route
-                path="/found"
-                element={
-                  <FoundItems
-                    setFoundItems={setFoundItems}
-                    setFoundLoading={setFoundLoading}
-                    foundItems={foundItems}
-                    foundLoading={foundLoading}
-                  />
-                }
-              />
-              <Route
-                path="/my-listing"
-                element={
-                  <MyListing
-                    setLostItems={setLostItems}
-                    setFoundItems={setFoundItems}
-                    setLostLoading={setLostLoading}
-                    setFoundLoading={setFoundLoading}
-                    lostItems={lostItems}
-                    foundItems={foundItems}
-                    lostLoading={lostLoading}
-                    foundLoading={foundLoading}
-                  />
-                }
-              />
-              {/* <Route path="/item/:id" element={<ItemDetails />} /> */}
-            </Routes>
-          {/* <Footer /> */}
+              <AnimatePresence>
+              <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Home />} />
+                <Route path="/Signup" element={<Signup />} />
+                <Route path="/Login" element={<Login />} />
+                <Route
+                  path="/lost"
+                  element={
+                    <LostItems
+                      setLostItems={setLostItems}
+                      setLostLoading={setLostLoading}
+                      lostItems={lostItems}
+                      lostLoading={lostLoading}
+                    />
+                  }
+                />
+                <Route
+                  path="/found"
+                  element={
+                    <FoundItems
+                      setFoundItems={setFoundItems}
+                      setFoundLoading={setFoundLoading}
+                      foundItems={foundItems}
+                      foundLoading={foundLoading}
+                    />
+                  }
+                />
+                <Route
+                  path="/my-listing"
+                  element={
+                    <MyListing
+                      setLostItems={setLostItems}
+                      setFoundItems={setFoundItems}
+                      setLostLoading={setLostLoading}
+                      setFoundLoading={setFoundLoading}
+                      lostItems={lostItems}
+                      foundItems={foundItems}
+                      lostLoading={lostLoading}
+                      foundLoading={foundLoading}
+                    />
+                  }
+                />
+              </Routes>
+              </AnimatePresence>
           </>
         )}
       </motion.div>      
-    </BrowserRouter>
   );
 }
 
