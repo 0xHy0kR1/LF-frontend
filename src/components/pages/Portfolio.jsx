f7be61b4aa9274e9f82750f79516dbcea967cbbc
0xHy0kR1 <helloweenhell666@gmail.com>
2025-02-19 09:59:30 +0530

    commit before editing project section of 'details of projects'

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -2,6 +2,16 @@ import React, { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Portfolio.css"; // Add necessary styles here
+import slide1 from "../../assets/slide1.jpg";
+import slide2 from "../../assets/slide2.jpg";
+import slide3 from "../../assets/slide3.jpg";
+import slide4 from "../../assets/slide4.jpg";
+import slide5 from "../../assets/slide5.jpg";
+import workSample from "../../assets/work-sample.jpg";
+import project1 from "../../assets/project1.jpg";
+import project2 from "../../assets/project2.jpg";
+import project3 from "../../assets/project3.jpg";
+
 
 const Portfolio = () => {
   const [showProjectDetails, setShowProjectDetails] = useState(false);
@@ -16,30 +26,44 @@ const Portfolio = () => {
   return (
     <div className="portfolio-container">
       {/* Carousel Section */}
-      <Carousel>
-        <Carousel.Item>
-          <img className="d-block w-100" src="/images/slide1.jpg" alt="First slide" />
+      <Carousel className="h-100 carousel-section">
+        <Carousel.Item className="carousel-item">
+          <img className="d-block w-100 carousel-img" src={slide1} alt="First slide" />
           <Carousel.Caption>
             <h3>Elegant Window Blinds</h3>
             <p>Choose the perfect blinds to match your home's aesthetics.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src="/images/slide2.jpg" alt="Second slide" />
+        <Carousel.Item className="carousel-item">
+          <img className="d-block w-100 carousel-img" src={slide2} alt="Second slide" />
           <Carousel.Caption>
             <h3>Modern Interior Design</h3>
             <p>Enhance your living space with premium designs.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src="/images/slide3.jpg" alt="Third slide" />
+        <Carousel.Item className="carousel-item">
+          <img className="d-block w-100 carousel-img" src={slide3} alt="Third slide" />
+          <Carousel.Caption>
+            <h3>Custom Solutions</h3>
+            <p>Tailor-made interiors designed to fit your lifestyle.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="carousel-item">
+          <img className="d-block w-100 carousel-img" src={slide4} alt="fouth slide" />
+          <Carousel.Caption>
+            <h3>Custom Solutions</h3>
+            <p>Tailor-made interiors designed to fit your lifestyle.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="carousel-item">
+          <img className="d-block w-100" src={slide5} alt="fifth slide" />
           <Carousel.Caption>
             <h3>Custom Solutions</h3>
             <p>Tailor-made interiors designed to fit your lifestyle.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-
+      <hr className="section-divider" />
       {/* Middle Section */}
       <div className="middle-section">
         <div className="text-content">
@@ -49,18 +73,19 @@ const Portfolio = () => {
             Get customized home interior solutions that reflect your style. Consult with us today and elevate your space.
           </p>
         </div>
-        <div className="image-content">
-          <img src="/images/work-sample.jpg" alt="Work Sample" />
+        <div className="middle-image">
+          <img src={workSample} alt="Work Sample"  className="work-sample-image"/>
         </div>
       </div>
-
+      <hr className="section-divider" />
+      
       {/* Details of Projects Section */}
       <div className="details-section">
         <h2>Details of Projects</h2>
         <div className="project-images">
-          <img src="/images/project1.jpg" alt="Project 1" />
-          <img src="/images/project2.jpg" alt="Project 2" />
-          <img src="/images/project3.jpg" alt="Project 3" />
+          <img src={project1} alt="Project 1" />
+          <img src={project2} alt="Project 2" />
+          <img src={project3} alt="Project 3" />
         </div>
         {showProjectDetails && (
           <div className="project-details animated">
